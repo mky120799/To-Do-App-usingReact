19482e30696172ce11c160a0e50c15477d926874
mky120799 <mky120799@gmail.com>
2024-02-19 23:16:11 +0530

    created container for the list added check boxes added css

---
+++ src/components/Button.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Button() {
+  return (
+   <button className='todo-button'>Add ToDo</button>
+  )
+}
---
+++ src/components/ToDoItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function 
+() {
+  return (
+    <div>
+        <li className='todo-item'>
+            <span>
+            <input type="checkbox"/>
+            <span>Eat</span>
+            </span>
+            <p>....</p>
+        </li>
+    </div>
+  )
+}
